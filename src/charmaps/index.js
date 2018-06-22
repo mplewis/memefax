b7624f539c2adac0a67d4c24c8fb5a04bc7db868
Matt Lewis <matt@mplewis.com>
2018-06-22 09:22:42 -0600

    charmap: regional indicator

--- src/charmaps/index.js
+++ src/charmaps/index.js
@@ -1,4 +1,5 @@
 export { default as bubble } from './bubble'
+export { default as regionalIndicator } from './regional_indicator'
 export { default as smallCaps } from './small_caps'
 export { default as superscript } from './superscript'
 export { default as upsideDown } from './upside_down'
